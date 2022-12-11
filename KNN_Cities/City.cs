a4a29a6ab6e029b78cdd41b0128b12c0ea19cbc9
bill <bemerson@forceamp.com>
2022-12-11 16:18:56 -0600

    v1

---
+++ KNN_Cities/City.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN_Cities
+{
+	public class City
+	{
+        [Name("id")]
+        [Index(0)]
+        public int Id { get; set; }
+        [Index(1)]
+        public string Name { get; set; }
+        [Index(2)]
+        public string State { get; set; }
+        [Index(3)]
+        public double Lat { get; set; }
+        [Index(4)]
+        public double Lng { get; set; }
+        [Index(5)]
+        public double Population { get; set; }
+    }
+}
+
---
+++ KNN_Cities/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using CsvHelper;
+using KNN_Cities;
+
+
+var usCities = new List<City>();
+
+using (var reader = new StreamReader("../../../uscities.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var records = csv.GetRecords<City>();
+
+    foreach (var r in records)
+    {
+        Console.WriteLine("{0} {1:N} {2:N}", r.Name , r.Lat, r.Lng);
+    }
+}
+
