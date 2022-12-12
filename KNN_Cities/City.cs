908eacd74ffa91ac4827570e09e5102e67966845
bill <bemerson@forceamp.com>
2022-12-12 09:59:19 -0600

    Bill

--- KNN_Cities/City.cs
+++ KNN_Cities/City.cs
@@ -13,15 +13,13 @@ namespace KNN_Cities
         [Index(0)]
         public int Id { get; set; }
         [Index(1)]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         [Index(2)]
-        public string State { get; set; }
+        public string State { get; set; } = null!;
         [Index(3)]
         public double Lat { get; set; }
         [Index(4)]
         public double Lng { get; set; }
-        [Index(5)]
-        public double Population { get; set; }
     }
 }
 
--- KNN_Cities/Program.cs
+++ KNN_Cities/Program.cs
@@ -3,17 +3,14 @@ using System.Globalization;
 using CsvHelper;
 using KNN_Cities;
 
-
-var usCities = new List<City>();
-
 using (var reader = new StreamReader("../../../uscities.csv"))
 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
 {
-    var records = csv.GetRecords<City>();
+    var usCities = csv.GetRecords<City>();
 
-    foreach (var r in records)
+    foreach (var r in usCities)
     {
-        Console.WriteLine("City {0} {1:N} {2:N}", r.Name , r.Lat, r.Lng);
+        Console.WriteLine("City {0},{1},{2:N} {3:N} ", r.Name , r.State, r.Lat, r.Lng);
     }
 }
 
